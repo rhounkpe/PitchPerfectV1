150d3fc0702c99cda594e9facae7d1b4ac48e106
Rufin Hounkpe <rhounkpe@gmail.com>
2020-07-19 15:18:41 +0200

    Pitch Perfect V1: initial state and adding Record button

---
+++ PitchPerfectV1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfectV1
+//
+//  Created by Rufin Hounkpe on 19/07/2020.
+//  Copyright © 2020 Digit Com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
