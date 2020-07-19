228771165fc7212739d3eb58071e5a2568aa4b1e
Rufin Hounkpe <rhounkpe@gmail.com>
2020-07-19 22:31:57 +0200

    Enable and Disable the Recording Buttons

--- PitchPerfectV1/ViewController.swift
+++ PitchPerfectV1/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
