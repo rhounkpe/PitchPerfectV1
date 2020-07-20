b3ab4d639bad4b04b426936aeeef29f02b8218dd
Rufin Hounkpe <rhounkpe@gmail.com>
2020-07-20 23:21:36 +0200

    Adding the AVAudioRecorder

--- PitchPerfectV1/RecordSoundsViewController.swift
+++ PitchPerfectV1/RecordSoundsViewController.swift
@@ -31,18 +31,14 @@ class RecordSoundsViewController: UIViewController {
         recordingLabel.text = "Recording in progress..."
         stopRecordingButton.isEnabled = true
         recordButton.isEnabled = false
-    }
-    
-    @IBAction func stopRecording(_ sender: Any) {
-        recordingLabel.text = "Recording in progress"
-        stopRecordingButton.isEnabled = true
-        recordButton.isEnabled = false
         
         
         let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
         let recordingName = "recordedVoice.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        print(filePath)
         
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
@@ -51,6 +47,17 @@ class RecordSoundsViewController: UIViewController {
         audioRecorder.isMeteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+        audioRecorder.stop()
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    
     }
 }
 
