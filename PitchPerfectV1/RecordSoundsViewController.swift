617c6895e649f4d400e42c92885a38f8c7641ee6
Rufin Hounkpe <rhounkpe@gmail.com>
2020-07-22 13:25:38 +0200

    Sending the Recorded Audio File

--- PitchPerfectV1/RecordSoundsViewController.swift
+++ PitchPerfectV1/RecordSoundsViewController.swift
@@ -60,7 +60,12 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     }
     
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-        print("finished recording")
+        if flag {
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else {
+            print("Recording was not successful!")
+        }
+        
     }
 }
 
